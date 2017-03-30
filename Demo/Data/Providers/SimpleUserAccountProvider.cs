98f227e049b7f98286ba9a820cbcddd79ba74d61
james mcfarland <james@arrochar.com>
2017-03-30 16:01:21 -0400

    Added Dispose support to Repository and Data layers.

--- Demo/Data/Providers/SimpleUserAccountProvider.cs
+++ Demo/Data/Providers/SimpleUserAccountProvider.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Nap.Demo.Data
 {
-    class SimpleUserAccountProvider : IUserAccountProvider
+    class SimpleUserAccountProvider : IUserAccountProvider, IDisposable
     {
         private readonly List<UserAccountDTO> _account = null;
         private int _nextId;
@@ -66,6 +66,41 @@ namespace Nap.Demo.Data
 
             return true;
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // jam2017 - do dispose here.
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~SimpleUserAccountProvider() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
 
         #endregion
     }
--- Demo/Repository/UserAccountRepository.cs
+++ Demo/Repository/UserAccountRepository.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Nap.Demo.Repository
 {
-    class UserAccountRepository : IUserAccountRepository
+    class UserAccountRepository : IUserAccountRepository, IDisposable
     {
         // internals - for the demo sample code, just using the Simple provider, use ProviderFactory to get it.
         private IUserAccountProvider _userAccountProvider = ProviderFactory.GetSimpleUserAccountProvider();
@@ -65,6 +65,41 @@ namespace Nap.Demo.Repository
             UserAccountDTO dto = new UserAccountDTO { Id = item.Id, Name = item.Name, Address = item.Address, Postal = item.Postal, Email = item.Email };
             return _userAccountProvider.Update(dto);
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // jam20170328 - do dispose here.
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~UserAccountRepository() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
         #endregion
     }
 }
