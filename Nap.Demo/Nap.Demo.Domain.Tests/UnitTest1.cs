058dd5d351d63f89d4c105c9cd267b7ebcc0c250
james mcfarland <james@arrochar.com>
2017-03-30 10:03:45 -0400

    Added Nap.Demo.Domain layer for NAP Demo Domain objects.

---
+++ Nap.Demo/Nap.Demo.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nap.Demo.Domain.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
