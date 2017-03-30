cdbcc1f4d2933462cc09e0d1c0c08aa322cacd06
james mcfarland <james@arrochar.com>
2017-03-30 13:48:09 -0400

    Added sample classes from client.  Adjusted the names to match so that UserAccount -> IUserAccountProvider -> UserAccountRepository

---
+++ Demo/Domain/UserAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nap.Demo.Domain
+{
+    public sealed class UserAccount
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Postal { get; set; }
+
+        public string Email { get; set; }
+    }
+}
