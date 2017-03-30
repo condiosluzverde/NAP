f3a4b4fa10a4fadf77668848fe4b014b985c5fd0
james mcfarland <james@arrochar.com>
2017-03-30 14:04:35 -0400

    Wired up sample code from client - made UserAccountRepository explicitly implement the IUserAccount interface. Added an IUseAccountProvider interface to the .Data layer. This will be used by Repository to inject the provider that it wants to use (possibly injectable from the client of UserAccountRepository as well.

--- Demo/Repository/IUserAccountRepository.cs
+++ Demo/Repository/IUserAccountRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Nap.Demo.Domain;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,37 @@ namespace Nap.Demo.Repository
 {
     interface IUserAccountRepository
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<UserAccount> GetAll();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        UserAccount GetUserAccount(int id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        UserAccount Add(UserAccount item);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        void Remove(int id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        bool Update(UserAccount item);
     }
 }
--- Demo/Repository/UserAccountRepository.cs
+++ Demo/Repository/UserAccountRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Nap.Demo.Domain;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace Nap.Demo.Repository
 {
-    class UserAccountRepository
+    class UserAccountRepository : IUserAccountRepository
     {
+        UserAccount IUserAccountRepository.Add(UserAccount item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<UserAccount> IUserAccountRepository.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        UserAccount IUserAccountRepository.GetUserAccount(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IUserAccountRepository.Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IUserAccountRepository.Update(UserAccount item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
