91267562ff346a912a05688e006755e0255b5bf8
james mcfarland <james@arrochar.com>
2017-04-03 16:30:52 -0400

    Added a plain old MVC web client for demo.

---
+++ Demo/Nap.Demo.WebClientMVCSPA/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Nap.Demo.WebClientMVCSPA.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Demo/Nap.Demo.WebClientMVCSPA/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Nap.Demo.WebClientMVCSPA.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
