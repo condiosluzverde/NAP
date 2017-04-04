1118c94b2a9b8ce3cf1fb0ef28b56adfb2c618c9
james mcfarland <james@arrochar.com>
2017-04-03 23:24:05 -0400

    Added yet another web client.

---
+++ Demo/Web/Nap.Demo.Web.Client/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nap.Demo.Web.Client.WebApiApplication" Language="C#" %>
---
+++ Demo/Web/Nap.Demo.Web.Client/userAccountsController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('userAccountsController', userAccountsController);
+
+    userAccountsController.$inject = ['$location']; 
+
+    function userAccountsController($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'userAccountsController';
+
+        activate();
+
+        function activate() { }
+    }
+})();
