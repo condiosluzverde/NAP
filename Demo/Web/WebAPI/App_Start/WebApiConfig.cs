1751e0d7206b557e08c60999c7564a11b7f234cc
james mcfarland <james@arrochar.com>
2017-04-06 16:11:06 -0400

    Added UserAccountController with scaffolding.

--- Demo/Web/WebAPI/App_Start/WebApiConfig.cs
+++ Demo/Web/WebAPI/App_Start/WebApiConfig.cs
@@ -15,7 +15,7 @@ namespace Nap.Demo.WebAPI
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
+                name: "DemoWebApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
