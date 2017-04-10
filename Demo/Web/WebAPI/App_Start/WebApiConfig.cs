6f897ae4b9d530f03fb862743c1ce8cf61482d61
james mcfarland <james@arrochar.com>
2017-04-09 21:42:42 -0400

    Added some comments to highlight the depency injection point (crude as it is) in the WebApiConfig.Register().

--- Demo/Web/WebAPI/App_Start/WebApiConfig.cs
+++ Demo/Web/WebAPI/App_Start/WebApiConfig.cs
@@ -10,6 +10,9 @@ namespace Nap.Demo.WebAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            // For the demo, explicitly get the simple UserAccount repo, this repo is not async due to no calls to db.
+            // The Register() method here is making a decision to inject the dependency on the SimpleUserAccountProvider as the UserAccount repository impl.
             config.Properties.TryAdd("UserAccountRepository", new Repository.UserAccountRepository(Data.ProviderFactory.GetSimpleUserAccountProvider()));
 
             // Web API routes
